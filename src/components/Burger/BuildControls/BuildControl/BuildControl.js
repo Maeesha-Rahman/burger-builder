179ff4e3026994131fc172fc92f9f9f3eae1c363
Maeesha <maeesha.rahman@gmail.com>
2020-07-23 17:29:37 -0400

    added functionality for adding and removing ingredients as well as calculating price

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import buildControls from '../BuildControls';
+import classes from './BuildControl.css';
 
-const buildControl = (props) => {
-
-};
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button onClick={props.removed} disabled={props.disabled} className={classes.Less}>Less</button>
+        <button onClick={props.added} className={classes.More}>More</button>
+    </div>
+);
 
 export default buildControl;
